a99bc1a4c9d69fd636b72df673fb90d3b3b2eba2
gaboquerizo <gabrielbaquerizo2000@gmail.com>
2024-09-02 12:29:55 -0500

    Sidebar: Funcionalidad de toggle

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Archivo index.js conectado');
+'use strict'
+
+// data-sidebar ( sidebar menu )
+// data-sidebar-toggler (boton cerrar sidebar)
+// data-sidebar-overlay (fondo oscuro)
+
+const $ = element => document.querySelector(element)
+const $$ = elements => document.querySelectorAll(elements)
+
+const sidebar = $('[data-sidebar]');
+const sidebarTogglers = $$('[data-sidebar-toggler]');
+const overlay = $('[data-sidebar-overlay]');
+
+function addEventOnElements(elements, eventType, callback) {
+    elements.forEach(element => element.addEventListener(eventType, callback));
+}
+
+addEventOnElements(sidebarTogglers, 'click', function(){
+    sidebar.classList.toggle('active');
+    overlay.classList.toggle('active');
+})
